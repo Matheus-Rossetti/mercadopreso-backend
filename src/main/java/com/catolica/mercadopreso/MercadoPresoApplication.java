5aae14b160e5769c316941f5de7434030bb234f6
Matheus-Rossetti <matheuzdreher@gmail.com>
2025-02-27 09:00:04 -0300

    Project start

---
+++ src/main/java/com/catolica/mercadopreso/MercadoPresoApplication.java
@@ -0,0 +1,13 @@
+package com.catolica.mercadopreso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoPresoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MercadoPresoApplication.class, args);
+    }
+
+}
